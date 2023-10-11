b48fd7b2f221eddcb0b1a1e1fa548c32b267c5ac
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-10-12 02:57:54 +0530

    storing user data in monogodb

--- util/auth.js
+++ util/auth.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
+require("../db/database");
+const User = require("../model/UserSchema");
+
 router.get('/', (req, res) => {
     res.send("hello from server router");
 });
 
 router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.json({message: req.body});
+    const {name, email, phone}  = req.body;
+    
+    if(!name || !email || !phone){
+        return res.status(422).json({error : 'Please enter all the details'});
+    }
+
+    User.findOne({email : email})
+    .then((userExists) => {
+        if(userExists){
+            return res.status(422).json({error : "User already exists"});
+        }
+
+        const user = new User({name: name, email, phone});
+
+        user.save().then(() => {
+            res.status(201).json({message : "user registration successfully"});
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    })
+    
+
 });
+
 
 
 module.exports = router;
